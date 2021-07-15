35d2369e6e9ca775bfdaabf23046d26dfcc08f5d
sergeynikitin <sergeynikitin2020@icloud.com>
2021-07-15 14:19:39 +0300

    new Commit

--- src/main/java/ServerApp.java
+++ src/main/java/ServerApp.java
@@ -1,15 +1,18 @@
+import lombok.extern.slf4j.Slf4j;
 import server.Handler;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+@Slf4j
 public class ServerApp {
     public static void main(String[] args) {
         try {
             ServerSocket socket = new ServerSocket(8080);
             System.out.println("Server is running");
             System.out.println("Waiting for connection");
+            log.debug("Start");
             while (true) {
                 Socket accept = socket.accept();
                 System.out.println("Client connection");
