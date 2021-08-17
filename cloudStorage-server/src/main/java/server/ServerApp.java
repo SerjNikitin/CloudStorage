32f2af76056cb4a0dc4939706cd2e6c2f6888afe
sergeynikitin <sergeynikitin2020@icloud.com>
2021-08-17 23:11:45 +0300

    refactoring

--- cloudStorage-server/src/main/java/server/ServerApp.java
+++ cloudStorage-server/src/main/java/server/ServerApp.java
@@ -6,7 +6,6 @@ public class ServerApp {
     public static void main(String[] args) {
         Flyway flyway = Flyway.configure().dataSource(
                 "jdbc:mysql://localhost:3306/cloud_storage", "root", "root").load();
-
         flyway.migrate();
         Server server = new Server();
     }
